8a72b5743e73738775b8edd2a15e12857a1dbbb9
heedong12 <dlgmldus3289@naver.com>
2024-07-24 07:10:44 +0900

    feat: connect main api

--- src/views/mobiles/MobileMainView.vue
+++ src/views/mobiles/MobileMainView.vue
@@ -1,13 +1,33 @@
 <script setup>
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 import IconBoothTino from '@/components/icons/mobiles/IconBoothTino.vue';
 import IconBannerArrow from '@/components/icons/mobiles/IconBannerArrow.vue';
-import { useRouter } from 'vue-router';
 import IconReserveTino from '@/components/icons/mobiles/IconReserveTino.vue';
+import { useRouter } from 'vue-router';
+import { useBoothUser } from '@/stores/mobiles/boothUser';
+import { useBoothList } from '@/stores/booths/boothList';
+import { useUser } from '@/stores/user';
 
 const router = useRouter();
+const useUserStore = useUser();
+const useBoothUserStore = useBoothUser();
 
-const handleRouter = (page, id) => {
-  router.push({ path: `/admin/${id}/${page}` });
+const { userOwnBoothId } = storeToRefs(useUserStore);
+const { userBoothInfo } = storeToRefs(useBoothUserStore);
+
+onMounted(async () => {
+  await useBoothUserStore.getBoothId();
+  useBoothUserStore.findUserBoothInfo(userOwnBoothId);
+});
+
+const handleClickAdminMenu = (type) => {
+  if (type === 'booth') {
+    router.push({ name: '' });
+  }
+  if (type === 'reserve') {
+    router.push({ name: '' });
+  }
 };
 </script>
 
@@ -15,10 +35,14 @@ const handleRouter = (page, id) => {
   <div class="flex flex-col justify-center items-center w-full h-full gap-10 dynamic-padding">
     <div class="flex flex-col gap-[30px] text-secondary-700 w-full">
       <p class="font-semibold text-xl">부스 등록 및 수정</p>
-      <div class="bg-primary-900 rounded-[20px] flex flex-col items-center w-full relative h-[190px] justify-end p-5">
+      <div
+        class="bg-primary-900 rounded-[20px] flex flex-col items-center w-full relative h-[190px] justify-end p-5 cursor-pointer"
+        @click="handleClickAdminMenu('booth')"
+      >
         <IconBoothTino class="absolute -top-7" />
         <div class="bg-white flex rounded-[20px] justify-between w-full h-[88px] p-5 items-center">
           <div class="flex flex-col gap-[6px]">
+            <!-- TODO: 부스 아이디 가져오는 api 오류 해결되면 수정 -->
             <p class="font-semibold">메카트로닉스공학부</p>
             <p class="text-xs">부스 운영을 응원합니다!</p>
           </div>
@@ -28,10 +52,14 @@ const handleRouter = (page, id) => {
     </div>
     <div class="flex flex-col gap-[30px] text-secondary-700 w-full">
       <p class="font-semibold text-xl">예약관리</p>
-      <div class="bg-primary-900 rounded-[20px] flex flex-col items-center w-full relative h-[190px] justify-end p-5">
+      <div
+        class="bg-primary-900 rounded-[20px] flex flex-col items-center w-full relative h-[190px] justify-end p-5 cursor-pointer"
+        @click="handleClickAdminMenu('reserve')"
+      >
         <IconReserveTino class="absolute -top-7" />
         <div class="bg-white flex rounded-[20px] justify-between w-full h-[88px] p-5 items-center">
           <div class="flex flex-col gap-[6px]">
+            <!-- TODO: 부스 아이디 가져오는 api 오류 해결되면 수정 -->
             <p class="font-semibold">25팀 대기 중</p>
             <p class="text-xs">예약 서비스 운영 시간 10:00 ~ 22:00</p>
           </div>
