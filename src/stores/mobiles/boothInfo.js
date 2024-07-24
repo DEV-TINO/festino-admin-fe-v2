23f60bd4873a626c22402b9c163142056782cf44
heedong12 <dlgmldus3289@naver.com>
2024-07-24 11:42:18 +0900

    feat: connect update booth info

--- src/stores/mobiles/boothInfo.js
+++ src/stores/mobiles/boothInfo.js
@@ -1,6 +1,11 @@
 import { defineStore } from 'pinia';
+import { api } from '@/utils/api';
 import { ref } from 'vue';
 
 export const useBoothInfo = defineStore('boothInfo', () => {
-  return {};
+  const updateBoothInfo = async (type, boothData) => {
+    const response = await api.put(`/admin/booth/${type}`, boothData);
+    console.log(response.data);
+  };
+  return { updateBoothInfo };
 });
