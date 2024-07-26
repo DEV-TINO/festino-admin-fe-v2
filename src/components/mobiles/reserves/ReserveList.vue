8148b3636138940cdaac1291c52b30d6b090b5e8
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-26 21:40:03 +0900

    refactor : get datas from reserve list

--- src/components/mobiles/reserves/ReserveList.vue
+++ src/components/mobiles/reserves/ReserveList.vue
@@ -1,44 +1,76 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { useReserveStore } from "@/stores/mobiles/reserve/reserveStore.js";
+import { ref, watch, onMounted } from "vue";
 import { storeToRefs } from "pinia";
+import { useReserveList } from "@/stores/reserve/reserveList";
+import { prettyDate } from "@/utils/utils";
+import { useUser } from '@/stores/user.js';
 import { useReserveModal } from '@/stores/mobiles/reserve/reserveModal';
+import IconNotFound from '../../icons/IconNotFound.vue';
 
-const { reserveData } = storeToRefs(useReserveStore());
-const { getReserveData } = useReserveStore();
-const { handleClickOpenAdmin } = useReserveModalStore();
-const type = ref("reserve")
+const props = defineProps({
+  listType: {
+    type: String,
+    required: true
+  }
+});
+
+const { getReserveList } = useReserveList();
+const { getUserOwnBoothId } = useUser();
+const { reserveList } = storeToRefs(useReserveList());
+const { userOwnBoothId } = storeToRefs(useUser());
+const { openReservePopup } = useReserveModal();
+const reserveData = ref([]);
+const loading = ref(false);
 
 const getMobileNum = (num) => {
-  return `${num.slice(0, 3)}-${num.slice(3, 7)}-${num.slice(7, 10)}`
+  return `${num.slice(0, 3)}-${num.slice(3, 7)}-${num.slice(7, 11)}`
 };
 
-const getReserveTime = (timeData) => {
-  const date = timeData.split("T");
-  const time = date[1].split(":");
-  return `${date[0]} ${time[0]}:${time[1]}`;
+const handleClickOpenReservePopup = (data) => {
+  openReservePopup(props.listType, data);
 };
 
-onMounted(() => {
-  getReserveData(type.value);
+watch(() => props.listType, async() => {
+  reserveData.value = [];
+  loading.value = true;
+  await getReserveList({ boothId: userOwnBoothId.value, type: props.listType });
+  if (props.listType === "reserve") reserveData.value = reserveList.value.reserve;
+  else if (props.listType === "cancel") reserveData.value = reserveList.value.cancel;
+  else if (props.listType === "complete") reserveData.value = reserveList.value.complete;
+  loading.value = false;
+});
+
+watch(() => reserveList.value, () => {
+  if (props.listType === "reserve") reserveData.value = reserveList.value.reserve;
+  else if (props.listType === "cancel") reserveData.value = reserveList.value.cancel;
+  else if (props.listType === "complete") reserveData.value = reserveList.value.complete;
+});
+
+onMounted(async () => {
+  loading.value = true;
+  await getUserOwnBoothId();
+  await getReserveList({ boothId: userOwnBoothId.value, type: props.listType });
+  reserveData.value = reserveList.value.reserve;
+  loading.value = false;
 });
 </script>
 
 <template>
   <div class="w-full flex flex-col">
-    <div class="flex items-center w-full py-[14px]" v-for="data in reserveData" :key="data">
+    <div v-if="loading">loading</div>
+    <div v-else class="flex items-center w-full py-[14px]" v-for="data in reserveData" :key="data.reservationNum">
       <div class="w-full flex text-secondary-700-light items-center">
         <div class="w-1/12"></div>
         <div class="w-1/12 text-xl font-semibold text-center">{{ data.reservationNum }}</div>
-        <div class="w-2/12 text-sm flex flex-col gap-1">
+        <div class="w-2/12 text-sm flex flex-col gap-1 items-center">
           <div>{{ data.userName }}</div>
           <div>{{ data.personCount }}명</div>
         </div>
-        <div class="w-5/12">
+        <div class="w-5/12 items-center flex flex-col">
           <div class="text-sm">{{ getMobileNum(data.phoneNum) }}</div>
-          <div class=" text-secondary-900 text-xs">{{ getReserveTime(data.updateAt) }}</div>
+          <div class=" text-secondary-900 text-xs">{{ prettyDate(data.updateAt) }}</div>
         </div>
-        <div class="w-2/12 flex justify-end">
+        <div class="w-2/12 flex justify-center">
           <div @click="handleClickOpenReservePopup(data)" class="px-4 py-1 rounded-full bg-restore text-primary-900">관리</div>
         </div>
         <div class="w-1/12"></div>
@@ -51,4 +83,6 @@ onMounted(() => {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+</style>
