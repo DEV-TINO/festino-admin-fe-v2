f7fba025b64e4b8f6839cde02d015a36c7b68e75
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-26 21:39:11 +0900

    style : change some layouts

--- src/views/mobiles/MobileReserveView.vue
+++ src/views/mobiles/MobileReserveView.vue
@@ -1,15 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import { storeToRefs } from "pinia";
-import { useReserveModalStore } from "@/stores/mobiles/reserve/reserveModalStore";
 import ReserveList from "@/components/mobiles/reserves/ReserveList.vue";
-import DeleteList from "@/components/mobiles/reserves/DeleteList.vue";
-import completeList from "@/components/mobiles/reserves/CompleteList.vue";
-import RestoreConfirmModal from "@/components/mobiles/reserves/RestoreConfirmModal.vue";
-import ReserveAdminModal from '@/components/mobiles/reserves/ReserveAdminModal.vue';
-import ReserveConfirmModal from "@/components/mobiles/reserves/ReserveConfirmModal.vue";
-
-const { restoreConfirmModalState, adminModalState, reserveConfirmModalState } = storeToRefs(useReserveModalStore());
 
 const isActive = ref({
   reserveList: true,
@@ -17,16 +8,19 @@ const isActive = ref({
   completeList: false,
 });
 
+const listType = ref("reserve");
+
 const toggleTab = (type) => {
-  if (type === "reserveList") {
+  listType.value = type;
+  if (type === "reserve") {
     isActive.value.reserveList = true;
     isActive.value.deleteList = false;
     isActive.value.completeList = false;
-  } else if (type === "deleteList") {
+  } else if (type === "cancel") {
     isActive.value.reserveList = false;
     isActive.value.deleteList = true;
     isActive.value.completeList = false;
-  } else if (type === "completeList") {
+  } else if (type === "complete") {
     isActive.value.reserveList = false;
     isActive.value.deleteList = false;
     isActive.value.completeList = true;
@@ -39,12 +33,6 @@ const activeUnderline = (isActive) => {
   }
 };
 
-const activeCancelUnderline = (isActive) => {
-  if (isActive) {
-    return 'h-1 bg-danger rounded-full text-secondary-700'
-  }
-};
-
 const activeTab = (isActive) => {
   if (isActive) {
     return 'text-secondary-900'
@@ -53,53 +41,47 @@ const activeTab = (isActive) => {
 </script>
 
 <template>
-  <RestoreConfirmModal v-if="restoreConfirmModalState" />
-  <ReserveAdminModal v-if="adminModalState" />
-  <ReserveConfirmModal v-if="reserveConfirmModalState" />
   <div class="w-full h-full">
-    <div class="flex font-semibold text-xl border-b border-secondary-300 relative">
-      <div class="w-1/12"></div>
-      <div class="pr-5">
-        <div @click="toggleTab('reserveList')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['reserveList'])">
+    <div class="flex justify-center font-semibold text-xl border-b border-secondary-300 relative gap-5">
+      <div>
+        <div @click="toggleTab('reserve')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['reserveList'])">
           예약 목록
         </div>
         <div class="absolute -bottom-[2px] w-[90px]" :class="activeUnderline(isActive['reserveList'])"></div>
       </div>
-      <div class="pr-5">
-        <div @click="toggleTab('completeList')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['completeList'])">
+      <div>
+        <div @click="toggleTab('complete')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['completeList'])">
           완료 목록
         </div>
         <div class="absolute -bottom-[2px] w-[90px]" :class="activeUnderline(isActive['completeList'])"></div>
       </div>
       <div>
-        <div @click="toggleTab('deleteList')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['deleteList'])">
+        <div @click="toggleTab('cancel')" class="mb-2 w-[90px] text-center cursor-pointer" :class="activeTab(!isActive['deleteList'])">
           삭제 목록
         </div>
-        <div class="absolute -bottom-[2px] w-[90px]" :class="activeCancelUnderline(isActive['deleteList'])"></div>
+        <div class="absolute -bottom-[2px] w-[90px]" :class="activeUnderline(isActive['deleteList'])"></div>
       </div>
     </div>
     <div class="flex items-center w-full text-secondary-900 border-b border-secondary-300 py-4">
-      <div class="flex w-full">
+      <div class="flex w-full items-center">
         <div class="w-1/12"></div>
-        <div class="w-1/12 flex flex-col">
+        <div class="w-1/12 flex flex-col items-center">
           <div>예약</div>
           <div>번호</div>
         </div>
-        <div class="w-2/12 flex flex-col">
-          <div>예약자</div>
+        <div class="w-2/12 flex flex-col items-center">
+          <div>예약자 /</div>
           <div>인원수</div>
         </div>
-        <div class="w-5/12 flex flex-col">
-          <div>연락처</div>
+        <div class="w-5/12 flex flex-col items-center">
+          <div>연락처 /</div>
           <div>예약시간</div>
         </div>
-        <div class="w-2/12 text-end">예약관리</div>
+        <div class="w-2/12 text-center">예약관리</div>
         <div class="w-1/12"></div>
       </div>
     </div>
-    <ReserveList v-if="isActive['reserveList']" />
-    <DeleteList v-if="isActive['deleteList']" />
-    <completeList v-if="isActive['completeList']" />
+    <ReserveList :listType = "listType" />
   </div>
 </template>
 
