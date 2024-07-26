3bb824138abaf9bc3faba43683ab28d39ae9f4b9
김민지 <alswlk0306@gmail.com>
2024-07-26 17:37:35 +0900

    feat: handle booth images

---
+++ src/components/icons/mobiles/IconDelete.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+<template>
+  <div>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_b_2063_3459)">
+        <rect width="24" height="24" rx="12" fill="black" fill-opacity="0.5"/>
+        <path d="M16.999 7L6.99961 16.9994" stroke="white" stroke-width="2" stroke-linecap="round"/>
+        <path d="M16.999 17L6.99961 7.00059" stroke="white" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      <defs>
+        <filter id="filter0_b_2063_3459" x="-4" y="-4" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feGaussianBlur in="BackgroundImageFix" stdDeviation="2"/>
+          <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_2063_3459"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_2063_3459" result="shape"/>
+        </filter>
+      </defs>
+    </svg>
+  </div>
+</template>
+<style lang="scss" scoped></style>
