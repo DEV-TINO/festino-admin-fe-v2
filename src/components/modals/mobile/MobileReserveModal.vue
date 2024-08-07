f545cbd7787cba18ce471c3812eb7296c40eb70d
jzongyun <jeong.yun@tukorea.ac.kr>
2024-08-07 17:28:48 +0900

    feat : make x button on reserve modal

--- src/components/modals/mobile/MobileReserveModal.vue
+++ src/components/modals/mobile/MobileReserveModal.vue
@@ -27,9 +27,20 @@ const clickReserve = () => {
 </script>
 
 <template>
-    <div class="w-[380px] min-w-[325px] bg-white rounded-3xl flex flex-col items-center px-10 py-8 gap-5">
+    <div class="w-[380px] min-w-[325px] bg-white rounded-3xl flex flex-col items-center px-10 py-8">
+      <div class="flex w-full justify-end">
+        <svg @click="closeMobilePopup()" class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </div>
       <IconBoothInfo type="info" />
-      <div class="flex flex-col items-center gap-4 w-full">
+      <div class="flex flex-col items-center gap-4 w-full pt-5">
         <div class="font-bold text-xl text-secondary-700">예약 관리</div>
         <div class="flex w-full rounded-2xl text-sm border border-primary-700">
           <div class="flex flex-col rounded-l-2xl bg-restore border-r border-primary-700">
@@ -44,7 +55,7 @@ const clickReserve = () => {
           </div>
         </div>
       </div>
-      <div class="mt-2 flex gap-5 font-semibold text-xl w-full">
+      <div class="mt-2 flex gap-5 font-semibold text-xl w-full pt-5">
         <div v-if="popupType !== 'complete'" @click="clickComplete()" class="flex items-center justify-center w-full h-12 rounded-3xl bg-primary-900 text-white">
           입장
         </div>
