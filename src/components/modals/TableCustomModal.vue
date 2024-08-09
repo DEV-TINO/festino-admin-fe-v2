c79536cc90f4b76830cc5eea4d4dad10f63c5411
heedong12 <dlgmldus3289@naver.com>
2024-08-10 01:53:29 +0900

    feat: add table when click add table button

--- src/components/modals/TableCustomModal.vue
+++ src/components/modals/TableCustomModal.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { useTableDetail } from '@/stores/booths/tableDetail';
+import { nextTick, ref } from 'vue';
 
 const useTableDetailStore = useTableDetail();
 const { TABLE_INFO, closeTableDetailModal, submitTableDetail } = useTableDetailStore;
 
+const modalContainer = ref(null);
+
 const handleInputCustomTableNum = (event, index) => {
   TABLE_INFO[index].customNum = event.target.value;
+};
+
+const handleClickAddTableButton = async () => {
+  TABLE_INFO.push({ tableNum: TABLE_INFO.length + 1, customNum: '' });
+  await nextTick();
+  if (modalContainer.value) {
+    modalContainer.value.scrollTop = modalContainer.value.scrollHeight;
+  }
 };
 </script>
 
@@ -13,43 +24,52 @@ const handleInputCustomTableNum = (event, index) => {
   <form @submit.prevent="">
     <div
       id="modalContainer"
+      ref="modalContainer"
       class="w-[780px] h-[720px] flex flex-col justify-between items-center bg-white rounded-2xl overflow-y-auto p-10 gap-5 text-2xl"
     >
       <div
         class="w-full flex justify-between items-center gap-5 text-xl text-secondary-700 h-[60px] shrink-0 font-bold"
       >
         <div class="w-32 text-center flex-shrink-0">테이블 번호</div>
-        <div class="w-full">커스텀 테이블 번호</div>
+        <div class="w-full">커스텀 번호</div>
       </div>
       <div v-for="(table, index) in TABLE_INFO" :key="index" class="w-full flex justify-between items-center gap-5">
-        <div class="w-32 text-center flex-shrink-0">{{ table.tableNum }}번</div>
+        <div class="w-32 text-center flex-shrink-0">{{ table.tableNum }}</div>
         <input
           type="text"
           placeholder="커스텀 할 테이블 번호를 입력해주세요."
           @input="handleInputCustomTableNum($event, index)"
-          :value="table.tableNum"
+          :value="table.customNum"
           maxlength="100"
           class="w-full h-[60px] border border-gray-400 bg-white px-[20px] rounded-2xl active:border-primary-900"
         />
       </div>
       <!-- Buttons -->
-      <div class="w-full flex justify-end items-center gap-[10px]">
+      <div class="w-full flex justify-between items-center text-xl pt-10">
         <button
-          class="is-button is-outlined w-[100px] h-[50px] font-semibold text-xl"
-          type="button"
-          @click="closeTableDetailModal()"
+          @click="handleClickAddTableButton()"
+          class="text-secondary-900 border-1 border-secondary-900 h-[50px] rounded-2xl w-[150px] ml-8"
         >
-          취소
+          테이블 추가
         </button>
-        <button
-          class="is-button w-[100px] h-[50px] font-semibold text-xl"
-          type="submit"
-          ref="submit"
-          @click="submitTableDetail()"
-          autofocus
-        >
-          확인
-        </button>
+        <div class="flex gap-[10px]">
+          <button
+            class="is-button is-outlined w-[100px] h-[50px] font-semibold"
+            type="button"
+            @click="closeTableDetailModal()"
+          >
+            취소
+          </button>
+          <button
+            class="is-button w-[100px] h-[50px] font-semibold"
+            type="submit"
+            ref="submit"
+            @click="submitTableDetail()"
+            autofocus
+          >
+            확인
+          </button>
+        </div>
       </div>
     </div>
   </form>
