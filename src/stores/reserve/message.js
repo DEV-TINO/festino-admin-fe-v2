b6cf0accf490bc3789010a528ce8e6107ff3845f
jzongyun <jeong.yun@tukorea.ac.kr>
2024-09-02 14:28:51 +0900

    fix : fix custom message error

--- src/stores/reserve/message.js
+++ src/stores/reserve/message.js
@@ -4,14 +4,18 @@ import { alertError, api } from '@/utils/api';
 import { useBoothDetail } from '@/stores/booths/boothDetail';
 import { useMessageModal } from '@/stores/reserve/messageModal';
 import { useBaseModal } from '@/stores/baseModal';
+import { useReserveModal } from '../mobiles/reserve/reserveModal';
 
 export const useMessage = defineStore('message', () => {
   const useMessageModalStore = useMessageModal();
   const boothDeatilStore = useBoothDetail();
   const baseModalStore = useBaseModal();
+  const useReserveModalStore = useReserveModal();
 
+  const { closeMobilePopup, openLoadingModal } = useReserveModalStore;
   const { messageInfo } = storeToRefs(useMessageModalStore);
   const { boothInfo } = storeToRefs(boothDeatilStore);
+  const { selectedBooth } = storeToRefs(useReserveModalStore);
 
   const message = ref('');
   const customMessageList = ref([
@@ -28,11 +32,6 @@ export const useMessage = defineStore('message', () => {
       messageType: 2,
     },
   ]);
-
-  const openLoadingModal = () => {
-    baseModalStore.setModalType('loadingModal');
-    baseModalStore.openModal();
-  };
 
   const getMessage = async (boothId) => {
     try {
@@ -74,7 +73,8 @@ export const useMessage = defineStore('message', () => {
     if (!messageInfo.value.phoneNum || !messageInfo.value.userName || !boothInfo.value.adminName) {
       return alertError('메시지 전송에 실패했습니다.');
     }
-    openLoadingModal();
+    baseModalStore.setModalType('loadingModal');
+    baseModalStore.openModal();
     try {
       const response = await api.post('/admin/message/send', {
         phoneNum: messageInfo.value.phoneNum,
@@ -93,6 +93,31 @@ export const useMessage = defineStore('message', () => {
       alertError(error);
     } finally {
       baseModalStore.closeModal();
+    }
+  };
+
+  const sendMobileMessage = async (message, data) => {
+    if (!data.phoneNum || !data.userName || !selectedBooth.value.adminName) {
+      return alertError('메시지 전송에 실패했습니다.');
+    }
+    openLoadingModal();
+    try {
+      const response = await api.post('/admin/message/send', {
+        phoneNum: data.phoneNum,
+        userName: data.userName,
+        adminName: selectedBooth.value.adminName,
+        message,
+      });
+
+      if (response.data === 'SEND_SUCCESS') {
+        alert('메시지 전송에 성공했습니다.');
+      } else {
+      }
+    } catch (error) {
+      console.error(error);
+      alertError(error);
+    } finally {
+      closeMobilePopup();
     }
   };
 
@@ -121,5 +146,6 @@ export const useMessage = defineStore('message', () => {
     getMessage,
     sendMessage,
     saveCustomMessage,
+    sendMobileMessage,
   };
 });
