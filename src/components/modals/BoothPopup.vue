f82a9dc69fafeced6ea51017a5a3cb3ac14bd1a8
김민지 <alswlk0306@gmail.com>
2024-09-04 20:19:06 +0900

    design: modal UI size

--- src/components/modals/BoothPopup.vue
+++ src/components/modals/BoothPopup.vue
@@ -54,39 +54,39 @@ onMounted(() => {
   <form @submit.prevent="handleSubmit()">
     <div
       v-if="checkCorrectSelect()"
-      class="w-[480px] h-[312px] flex flex-col justify-between items-center bg-white rounded-2xl"
+      class="w-[400px] h-[260px] flex flex-col justify-between items-center bg-white rounded-2xl"
     >
       <div class="flex flex-col pt-[32px] items-center">
         <IconNotice :type="'info'" class="pb-[20px]" />
-        <div class="text-secondary-700 font-bold text-xl text-center">
+        <div class="text-secondary-700 font-bold text-md text-center">
           {{ modalInfo[seletedType][`${getBoothInfo()}`]?.title }}
         </div>
-        <div class="pt-[12px] text-secondary-700-light text-base whitespace-pre-wrap text-center">
+        <div class="pt-[12px] text-secondary-700-light text-sm whitespace-pre-wrap text-center">
           {{ modalInfo[seletedType][`${getBoothInfo()}`]?.subTitle }}
         </div>
       </div>
       <div class="flex justify-between gap-[20px] pb-[32px] w-full px-[40px]">
-        <button type="button" class="is-button is-outlined w-[190px] h-[48px]" @click="useBoothPopupStore.closePopup()">
+        <button type="button" class="is-button is-outlined rounded-[16px] w-1/2 h-[40px] text-sm" @click="useBoothPopupStore.closePopup()">
           취소
         </button>
-        <button type="submit" class="is-button w-[190px] h-[48px]" autofocus ref="submit">확인</button>
+        <button type="submit" class="is-button w-1/2 h-[40px] text-sm rounded-[16px]" autofocus ref="submit">확인</button>
       </div>
     </div>
 
     <!-- Error -->
     <div
       v-if="!checkCorrectSelect()"
-      class="w-[480px] h-[312px] flex flex-col justify-between items-center bg-white rounded-2xl"
+      class="w-[400px] h-[260px] flex flex-col justify-between items-center bg-white rounded-2xl"
     >
       <div class="flex flex-col pt-[32px] items-center">
         <IconNotice :type="'error'" class="pb-[20px]" />
-        <div class="text-secondary-700 text-xl text-center">{{ modalInfo.error.title }}</div>
-        <div class="pt-[12px] text-secondary-700-light text-base whitespace-pre-wrap text-center">
+        <div class="text-secondary-700 text-md text-center">{{ modalInfo.error.title }}</div>
+        <div class="pt-[12px] text-secondary-700-light text-sm whitespace-pre-wrap text-center">
           {{ modalInfo.error.subTitle }}
         </div>
       </div>
       <div class="flex justify-between gap-[20px] pb-[32px] w-full px-[40px]">
-        <button type="submit" class="is-button w-full h-[48px]" autofocus ref="submit">확인</button>
+        <button type="submit" class="is-button w-full h-[40px] text-sm rounded-[16px]" autofocus ref="submit">확인</button>
       </div>
     </div>
   </form>
