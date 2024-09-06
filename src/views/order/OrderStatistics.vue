810f055e07988df0c310467cdc6a2e94f7f58b2e
김민지 <alswlk0306@gmail.com>
2024-09-06 15:33:33 +0900

    fix: statistic date button error

--- src/views/order/OrderStatistics.vue
+++ src/views/order/OrderStatistics.vue
@@ -23,7 +23,7 @@ const { allOrderStatistics } = storeToRefs(useOrderStatisticsStore);
 
 const month = ref(9);
 const isLoading = ref(false);
-const day = ref(1);
+const day = ref(0);
 const type = ref('all');  // 기본 토글 선택 값을 'all'로 설정
 
 const myBooth = ref('');  // boothId와 일치하는 부스를 저장할 변수
@@ -68,11 +68,13 @@ const determineActiveDate = () => {
 
 const activeDate = ref(determineActiveDate());
 
-const handleButtonClick = (key) => {
+const handleButtonClick = async (key) => {
   if (key !== activeDate.value) {
     activeDate.value = key;
-    day.value = key;
+    day.value = activeDateMap[key];
+    console.log(day.value);
   }
+  await fetchStatistics();
 };
 
 const sortStrategies = {
@@ -95,7 +97,6 @@ const handleStatisticsSort = (sort) => {
 
 const fetchStatistics = async () => {
   isLoading.value = true;
-  day.value = activeDate.value;
   await getStatistics({
     boothId: boothId.value,
     date: day.value,
@@ -104,35 +105,9 @@ const fetchStatistics = async () => {
   isLoading.value = false;
 };
 
-// boothId 변경 시 통계 데이터를 다시 가져오기
-watchEffect(async () => {
-  if (boothId.value) {
-    myBooth.value = boothList.value.find(booth => booth.boothId === boothId.value);
-    activeDate.value = determineActiveDate();
-    await fetchStatistics({
-      boothId: boothId.value,
-      date: day.value,
-      type: type.value,
-    });
-  }
-});
-
-// activeDate 변경 시 통계 데이터를 다시 가져오기
-watchEffect(async () => {
-  await fetchStatistics({
-    boothId: boothId.value,
-    date: day.value,
-    type: type.value,
-  });
-});
-
 // type이 변경될 때마다 getStatistics 호출
 watchEffect(async () => {
-  await fetchStatistics({
-    boothId: boothId.value,
-    date: day.value,
-    type: type.value,
-  });
+  await fetchStatistics();
 });
 
 onMounted(async () => {
@@ -140,11 +115,7 @@ onMounted(async () => {
 
   // boothList에서 boothId와 일치하는 부스를 찾아서 myBooth에 저장
   myBooth.value = boothList.value.find(booth => booth.boothId === boothId.value);
-  await fetchStatistics({
-    boothId: boothId.value,
-    date: day.value,
-    type: type.value,
-  });
+  await fetchStatistics();
 });
 </script>
 
@@ -175,11 +146,16 @@ onMounted(async () => {
     <!-- 통계 영역 -->
     <div class="w-full h-[638px] flex overflow-x-scroll scrollbar-hide">
       <div class="w-1/2 min-w-[490px] pt-20 pl-10 h-full border border-primary-900 rounded-l-[20px]">
-        <StatisticsGraph />
+        <div v-if="isLoading" class="text-primary-900 font-semibold h-[500px] flex justify-center items-center">
+          Loading...
+        </div>
+        <div v-if="!isLoading">
+          <StatisticsGraph />
+        </div>
         <div class="flex justify-end items-center pr-4 ">
           <IconScroll class="w-5 h-5 mr-1" />
           <p class="text-xs text-gray-500 font-semibold">스크롤</p>
-      </div>
+        </div>
       </div>
       <div
         class="w-1/2 min-w-[490px] h-full overflow-hidden p-[40px] border-r border-y border-primary-900 rounded-r-[20px] flex flex-col justify-between items-center"
@@ -195,7 +171,7 @@ onMounted(async () => {
             class="min-w-[120px] w-[200px] h-[50px] is-button relative"
             v-for="(dateInfo, key) in formattedDateList"
             :key="key"
-            :class="{ 'is-outlined': key !== activeDate }"
+            :class="{ 'is-outlined': activeDateMap[dateInfo.day] !== day }"
             @click="handleButtonClick(dateInfo.day)"
           >
             {{ formattedMonth(month) }}/{{ dateInfo.day }} ({{ dateInfo.dateName }})
@@ -254,6 +230,7 @@ onMounted(async () => {
               </div>
             </p>
           </div>
+
           <div class="h-full overflow-y-scroll scrollbar-hide">
             <div
               class="flex flex-row justify-between font-normal pl-7 min-h-10 items-center border-b-1 border-primary-300 shrink-0 hover:bg-gray-200"
@@ -278,6 +255,7 @@ onMounted(async () => {
               </p>
             </div>
           </div>
+
           <div
             class="grid place-items-center text-primary-900 font-medium text-2xl h-[73px] rounded-b-3xl bottom-0 shrink-0 bg-white border-secondary-600 shadow-[0px_-5px_6px_0px_rgba(0,0,0,0.1)]"
           >
